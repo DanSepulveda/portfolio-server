dec35924fdff9dff1276d6c7cb5a826a56114c14
Daniel Sepúlveda <dansepdev@gmail.com>
2023-05-31 00:01:53 -0400

    service configuration ok

--- functions/src/config/app.js
+++ functions/src/config/app.js
@@ -1,5 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const {setRoutes} = require("./routes");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+setRoutes(app);
 
 module.exports = {app};
