08096d26c40342b33fb35aa463735e3be62c87bb
Daniel Sepúlveda <dansepdev@gmail.com>
2023-05-31 10:36:12 -0400

    file structure modified

---
+++ functions/config/routes.js
@@ -0,0 +1,13 @@
+const apiRoutes = require("./src/routes");
+
+module.exports.setRoutes = (app) => {
+  app.get("/", (req, res) => {
+    res.send("Welcome to the APP");
+  });
+
+  /**
+   * API Route.
+   * All the API will start with "/api/[MODULE_ROUTE]"
+   */
+  app.use("/api", apiRoutes);
+};
