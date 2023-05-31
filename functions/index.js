ef9f865fd2b11bce13604f332decae5164a9366d
Daniel Sepúlveda <dansepdev@gmail.com>
2023-05-31 10:35:04 -0400

    require path changed

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const {app} = require("./src/config/app");
+const {app} = require("./config/app");
 const functions = require("firebase-functions/v2");
 functions.setGlobalOptions({maxInstances: 1});
 
